7930b32bd69e3145e3fbc31ef5fa7ab350e5be4c
Daniel R <sadasant@gmail.com>
2011-09-11 05:38:10 +0000

    :) it works (Backbone I love you)

--- js/sdsnt.js
+++ js/sdsnt.js
@@ -1,34 +1,139 @@
-var S = {
-  author: "Daniel R. (sadasant.com)",
-  debug: _.bind(console.debug, console),
-
-  /* PAGE OBJECT */
-  Page: Backbone.Model.extend({
-    initialize: function(args) {
-      S.debug("Started page: "+args.name);
-    },
-    defaults: {
-      name: "ERROR",
-      data: "Nothing here, move away"
-    }
-  }),
-
-  /* MAIN PAGES */
-  Pages: Backbone.Collection.extend({
-    model: this.page,
-    url: 'http://jsbin.com/eyiyus',
-    find: function(name){
-      return this.filter(function(game) {  
-        return game.get('name') == name;  
+$(function(){
+  var S = (function(){
+    var author = "Daniel R. (sadasant.com)";
+  
+    /* BACKBONES */
+    var Page = Backbone.Model.extend({
+      initialize: function(args) {
+        S.debug("Started page: "+args.name);
+      },
+      defaults: {
+        name: "ERROR",
+        data: "Nothing here."
+      }
+    });
+    var Pages = Backbone.Collection.extend({
+      model: Page,
+      getData: function(name){
+        var page = this.find(function(page) { 
+          return page.get('name') === name;  
+        });
+        return page.get('data');
+      }
+    });
+    var Content = Backbone.View.extend({
+      events: {
+        "click img": "expand",
+        "click .email": "email"
+      },
+      render: function(page,data,title,parent){
+        $(this.el).fadeOut(140,function(){
+          S.debug(page);
+          $("a").removeClass("active");
+          if (S.menu !== null) {
+            $("menu").html(S.menu);
+            S.menu = null;
+          }
+          if (!title) {
+            $("#"+page).addClass("active"); }
+          else {
+            S.menu = $("menu").html(); 
+            $("menu").html(title+". <a href='#/"+parent+"'>Go back</a>.");
+          }
+          var html = "<p>"+data.content+"</p>";
+          if (data.linksTitle) {
+            html += "<b>"+data.linksTitle+"</b>";
+          }
+          if (data.links && typeof(data.links) !== "string") {
+            html += "<ul>";
+            for (var i in data.links){
+              var di = data.links[i];
+              var link = (i.match(/http:\/\//g))? i : "#/"+page+"/"+i;
+              if (di.title) html += "<li><a href='"+link+"' title='"+di.title+"'>"+di.list+"</a></li>";
+              else html += "<li>"+di.list+"</li>";
+            }
+          }
+          $(this).html(html);
+          $(this).fadeIn(140);
+        });
+      },
+      expanded: null,
+      expand: function(e){
+        $("#content img").each(function(){
+          $(this).animate({
+            width:50
+          });
+        });
+        if (this.expanded !== e.target){
+          this.expanded = e.target;
+          $(e.target).animate({
+            width:200
+          });
+        } else {
+          this.expanded = null;
+        }
+      },
+      email: function(e){
+        var my = "djrs",
+          mail = "sadasant",
+          html = "email: "+my+"@"+mail+".com";
+          $(".email").fadeOut(140,function(){
+            $(this).html(html).fadeIn();
+          });
+      }
+    
+    });
+    var Router = Backbone.Router.extend({
+      initialize: function(options){
+        this.collection = options.collection;
+        this.view = options.view;
+      },
+      routes: {
+        "/:page": "pages",
+        "/:page/:subpage": "subpages"
+      },
+      pages: function(page){
+        var data = this.collection.getData(page);
+        this.view.render(page,data);
+      },
+      subpages: function(page,subpage){
+        var data = this.collection.getData(page);
+        this.view.render(page+"/"+subpage,data.links[subpage],data.links[subpage].title,page);
+      }
+    });
+    /**/
+    
+    function start(){
+      $.ajax({
+        type:"GET",
+        url: 'http://jsbin.com/eyiyus/30',
+        success: function (json){
+          json = $.parseJSON(json);
+          for (var i in json) {
+            S.Pages.add({ name:i, data:json[i] });
+          }
+          S.Router = new Router({ collection:S.Pages, view: S.Content });
+          Backbone.history.start();
+        }
       });
     }
-  })
-};
 
-//var testpage = new S.Page({name: "about",data: _json.about});
-//S.debug(testpage.get('data'));
+    return {
+      debug: _.bind(console.debug, console),
+      Pages: new Pages(),
+      Content: new Content({ el: $("#content") }),
+      Router: null, //later
+      start: start,
+      menu: null
+    };
+  })();
+  
+  S.start();
+  //S.Pages.add({ name:"about", data:"lol" });
+  //var pageData = S.Pages.getData("about");
+  //S.debug(pageData);
+  //S.debug(page.prototype.get("data"));
 
-var testPages = new S.Pages();
-testPages.fetch();
-S.debug(testPages);
+});
+
 
